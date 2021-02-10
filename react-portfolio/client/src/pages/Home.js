0e0c8a30827e3942934a3226de3644137b63ad17
Dustin LaMarr <dustin.lamarr@gmail.com>
2021-02-10 15:20:01 -0700

    Updated server.js for production build

--- react-portfolio/client/src/pages/Home.js
+++ react-portfolio/client/src/pages/Home.js
@@ -1,17 +1,35 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import JumboTron from "../components/Jumbotron";
-import CardComp from "../components/CardComp";
+import JumboTron from "react-bootstrap/Jumbotron";
+import CardDeck from "react-bootstrap/CardDeck";
+import Card from "react-bootstrap/Card";
 
 const Home = () => {
   return (
     <Container>
       <JumboTron>
-<CardComp id="cardComponent">
-
-</CardComp>
+        <CardDeck>
+          <Card id="projects">
+            <Card.Body>
+              <Card.Img src="../images.code.png"></Card.Img>
+            </Card.Body>
+            <Card.Footer as="h3">Projects.</Card.Footer>
+          </Card>
+          <Card id="resume">
+            <Card.Body></Card.Body>
+            <Card.Footer as="h3">Resume.</Card.Footer>
+          </Card>
+          <Card id="writing">
+            <Card.Body></Card.Body>
+            <Card.Footer as="h3">Writing.</Card.Footer>
+          </Card>
+          <Card id="me">
+            <Card.Body></Card.Body>
+            <Card.Footer as="h3">About Me.</Card.Footer>
+          </Card>
+        </CardDeck>
       </JumboTron>
-      </Container>
+    </Container>
   );
 };
 
