9e2d27a2299e2e5883a93911156bdb81adc51b1a
Dustin LaMarr <dustin.lamarr@gmail.com>
2021-02-26 11:43:04 -0700

    Troubleshooting server

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const PORT = process.env.PORT || 5001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
