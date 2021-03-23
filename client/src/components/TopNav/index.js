f8ea2b161087cc11834a0309c088182f461346a0
Dustin LaMarr <dustin.lamarr@gmail.com>
2021-03-23 08:32:27 -0600

    Added IG and LI icons. More cosmetic changes, cleaning up files.

--- client/src/components/TopNav/index.js
+++ client/src/components/TopNav/index.js
@@ -5,17 +5,23 @@ function TopNav() {
   return (
     <nav className="navbar nav-bg">
    
-        <div className="row align-items-center justify-content-center">
-          <div className="col d-inline align-self-center pl-3">
+        <div className="row align-items-center justify-content-center g-0">
+          <div className="col d-inline align-self-center">
         <a className="navbar-brand" href="/" alt="deezus">
               <img src="/images/deez.png" alt="" height="75" width="75"></img>
             </a>
             </div>
             <div className="col d-inline align-self-end p-0">
-          <a className="navbar-brand" href="">
+          <a className="navbar-brand mr-1" href="https://www.instagram.com/prodigal_punk/">
             <img src="/images/ig.svg" alt="instagram logo" height="30" width="30" />
           </a>
           </div>
+          <div className="col d-inline align-self-end p-0">
+          <a className="navbar-brand" href="https://www.linkedin.com/in/dustinlamarr/">
+            <img src="/images/li.svg" alt="linkedin logo" height="30" width="30" />
+          </a>
+          </div>
+          
           </div>
     </nav>
   );
